b765258f325a2812b89aa7c9c320a54c8f4d9097
yangyao <y.yang@aftership.com>
2023-06-06 16:59:34 +0800

    Remove IntegrationTrait

--- Setup/RecurringData.php
+++ Setup/RecurringData.php
@@ -7,11 +7,11 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Integration\Api\IntegrationServiceInterface;
 use Magento\Integration\Api\AuthorizationServiceInterface;
 use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Integration\Model\Integration;
 use Magento\Store\Api\StoreRepositoryInterface;
 
 class RecurringData implements InstallDataInterface
 {
-    use IntegrationTrait;
 
     /**
      * @var StoreRepositoryInterface
@@ -47,6 +47,45 @@ class RecurringData implements InstallDataInterface
 
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $this->run($setup, $context);
+		$storeList = $this->storeRepository->getList();
+		$integrationNames = [];
+		foreach ($storeList as $index => $item) {
+			$storeId = $item->getId();
+			if ($storeId == 0) continue;
+			foreach ($this->apps as $app) {
+				$this->createIntegration($this->buildIntegrationData($app, $storeId, $item->getCode()));
+			}
+		}
     }
+
+	private function buildIntegrationData($app, $storeId, $storeCode)
+	{
+		$name = sprintf("AfterShip %s For Store: %s", ucfirst($app), $storeCode);
+		$identityLinkUrl = sprintf("https://accounts.aftership.com/oauth/%s/magento-2/identity", $app);
+		$endpoint = sprintf("https://accounts.aftership.com/oauth/%s/magento-2/callback?store_id=%d", $app, $storeId);
+		if ($app === 'tracking') {
+			$endpoint = sprintf("https://accounts.aftership.com/oauth/magento-2/callback?store_id=%d", $storeId);
+			$identityLinkUrl = 'https://accounts.aftership.com/oauth/magento-2/identity';
+		}
+		$integrationData = [
+			'name' => $name,
+			'email' => 'apps@aftership.com',
+			'endpoint' => $endpoint,
+			'identity_link_url' => $identityLinkUrl
+		];
+		return $integrationData;
+	}
+
+	private function createIntegration($integrationData)
+	{
+		$integration = $this->integrationService->findByName($integrationData['name']);
+		if ($integration->getId()) {
+			$integrationData[Integration::ID] = $integration->getId();
+			$this->integrationService->update($integrationData);
+		} else {
+			$integration = $this->integrationService->create($integrationData);
+		}
+		$this->authorizationService->grantAllPermissions($integration->getId());
+		return $integration;
+	}
 }
