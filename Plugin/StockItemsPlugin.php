fd43f39519d7e620208c0f30a4e531cd71249cee
yangyao <y.yang@aftership.com>
2024-01-24 11:11:49 +0800

    Add skus filter for stockitems

--- Plugin/StockItemsPlugin.php
+++ Plugin/StockItemsPlugin.php
@@ -2,6 +2,8 @@
 
 namespace AfterShip\Tracking\Plugin;
 
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\CatalogInventory\Api\Data\StockItemInterface;
 use Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory;
 use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
@@ -30,21 +32,33 @@ class StockItemsPlugin
      */
     protected $criteriaFactory;
 
+    /**
+     * CollectionFactory instance
+     *
+     * @var CollectionFactory $productCollectionFactory
+     */
+    protected $productCollectionFactory;
+
 
     /**
+     * StockItemsPlugin constructor.
+     *
+     * @param RequestInterface $request
      * @param StockItemRepositoryInterface $stockItemRepository
      * @param StockItemCriteriaInterfaceFactory $criteriaFactory
+     * @param CollectionFactory $productCollectionFactory
      */
     public function __construct(
         RequestInterface                  $request,
         StockItemRepositoryInterface      $stockItemRepository,
-        StockItemCriteriaInterfaceFactory $criteriaFactory
-
+        StockItemCriteriaInterfaceFactory $criteriaFactory,
+        CollectionFactory                 $productCollectionFactory
     )
     {
         $this->request = $request;
         $this->stockItemRepository = $stockItemRepository;
         $this->criteriaFactory = $criteriaFactory;
+        $this->productCollectionFactory = $productCollectionFactory;
     }
 
     /**
@@ -66,9 +80,53 @@ class StockItemsPlugin
     )
     {
         $productIds = $this->request->getParam('productIds');
-        if (empty($productIds)) {
-            return $proceed($scopeId, $qty, $currentPage, $pageSize);
+        if (!empty($productIds)) {
+            return $this->getStockItemsByProductIds($productIds, $currentPage, $pageSize);
         }
+        $skus = $this->request->getParam('skus');
+        if (!empty($skus)) {
+            return $this->getStockItemsBySkus($skus, $currentPage, $pageSize);
+        }
+        return $proceed($scopeId, $qty, $currentPage, $pageSize);
+    }
+
+    /**
+     * get stock items by skus.
+     *
+     * @param string|string[] $skus
+     * @param int $currentPage
+     * @param int $pageSize
+     * @return StockItemInterface[]
+     */
+    private function getStockItemsBySkus($skus, $currentPage, $pageSize)
+    {
+        if (is_array($skus)) {
+            $skus = array_map('strval', $skus);
+        } else {
+            $skus = array_map('strval', explode(',', $skus));
+        }
+        /** @var $productCollection Collection */
+        $productCollection = $this->productCollectionFactory->create();
+        $productCollection->addAttributeToFilter('sku', ['in' => $skus])
+            ->addAttributeToSelect('entity_id');
+        $productIds = $productCollection->getColumnValues('entity_id');
+        $criteria = $this->criteriaFactory->create();
+        $criteria->setLimit($currentPage, $pageSize);
+        $criteria->setScopeFilter(0);
+        $criteria->setProductsFilter($productIds);
+        return $this->stockItemRepository->getList($criteria);
+    }
+
+    /**
+     * get stock items by product ids.
+     *
+     * @param string|string[] $productIds
+     * @param int $currentPage
+     * @param int $pageSize
+     * @return StockItemInterface[]
+     */
+    private function getStockItemsByProductIds($productIds, $currentPage, $pageSize)
+    {
         if (is_array($productIds)) {
             $productIds = array_map('intval', $productIds);
         } else {
